25110b7e14f893b0bbf54f0e521e22a948bce749
Krishnan <krish.nair101@gmail.com>
2017-04-09 17:19:57 +1200

    javaScript scripts

---
+++ JS-Basics/script.js
@@ -0,0 +1,31 @@
+/*console.log('Hello World');
+
+var name = "John";
+console.log(name);
+
+var lastName = "Smith";
+console.log(lastName);
+
+var age = 28;
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);*/
+
+var name    = 'John';
+var age     = 28;
+console.log(name + ' ' + age);
+console.log(age + age);
+
+var job, isMarried;
+
+job     = "Teacher";
+isMarried = false;
+
+console.log(name + name);
+console.log(name + ' ' + ' ' + lastName + 'is a ' + age + 'years old ' + job + '. Is he married?' + ' ' + false + '.');
+
+var lastName    = prompt('What is your last name?');
+console.log(lastName);
+
+alert(name + ' ' + ' ' + lastName + ' is a ' + age + 'years old ' + job + '. Is he married?' + ' ' + false + '.');
